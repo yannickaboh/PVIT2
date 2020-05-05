ec0ba3a2eedd31b30a1853e66aa5b625179c37ff
yannickaboh <yannickabohthierry@gmail.com>
2020-05-05 22:10:09 +0100

    update utils

--- paiement/utils.py
+++ paiement/utils.py
@@ -1,5 +1,5 @@
 import random
 import string
 
-def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+def random_string_generator(size=9, chars=string.ascii_uppercase + string.digits, core='VERA'):
+    return core.join(random.choice(chars) for _ in range(size))
